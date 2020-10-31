c7970b1ab34f4464ee8fa7004a770769edfe41b9
Rasen0000 <rasenou@ya.ru>
2020-10-31 21:28:13 +0300

    1

--- src/ru/example/Main.java
+++ src/ru/example/Main.java
@@ -1,34 +1,38 @@
 package ru.example;
 
+import java.util.Objects;
+
+import static java.lang.Character.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         //System.exit(2);
 
-        task1();
-        task2();
-        getMaxNumber();
-        isCharA();
-
+        isCharNumber();
+        task9();
+        task10();
+        task11();
+        task12();
     }
 
-    public static byte task1() {
-        byte a = 2;
-        short b = 1002;
+    public static byte task1(byte a, short b) {
+         a = 2;
+         b = 1002;
         System.out.println((byte) (a + b));
         return 0;
     }
 
-    public static int task2() {
-        int a = 212;
-        long b = 22222222;
+    public static int task2(int a, long b) {
+         a = 212;
+         b = 22222222;
         System.out.println( a * b);
         return 0;
     }
 
-    public static void getMaxNumber () {
-        int a = 100;
-        int b = 140;
+    public static void getMaxNumber (int a, int b) {
+         a = 100;
+         b = 140;
         if (a>b){
             System.out.println("Максимум a = " + a);
         }
@@ -41,14 +45,46 @@ public class Main {
 
     }
 
-    public static boolean isCharA() {
-        char a = 'А';
-        switch (a){
-            case 'A': return true;
-            default:
-                return false;
+    static int isCharA(char a, char A) {
+
+         a = 'А';
+         A = 'А';
+
+            System.out.println(Objects.equals(a, A));
+        return 0;
         }
 
+    static int isCharNumber() {
+        char ch = '2';
+        System.out.println(isDigit(ch));
+        return 0;
     }
 
+    public static void task9() {
+
+    for (int i=0; i<30;i++)
+        System.out.println(Integer.toBinaryString(i));
+    }
+
+    public static void task10 () {
+        int maxValue = Integer.MAX_VALUE;
+
+        System.out.println(Integer.toBinaryString(maxValue));
+        System.out.println(Integer.toBinaryString(maxValue + 1));
+    }
+    static int task11(){
+        char ch = 'x';
+        System.out.println(Character.digit(ch, 36));
+        return 0;
+    }
+
+    static int task12(){
+        int ch = 14;
+        System.out.println(Character.forDigit(ch, 36));
+        return 0;
+    }
+
+
 }
+
+
