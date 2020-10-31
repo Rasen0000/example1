cc8b4838e47457938cf58164752a221e345cfce2
Rasen0000 <rasenou@ya.ru>
2020-10-31 13:31:33 +0300

    1

--- src/ru/example/Main.java
+++ src/ru/example/Main.java
@@ -7,25 +7,48 @@ public class Main {
 
         task1();
         task2();
+        getMaxNumber();
+        isCharA();
 
     }
 
     public static byte task1() {
         byte a = 2;
-        int b = 1002;
+        short b = 1002;
         System.out.println((byte) (a + b));
         return 0;
     }
 
-    public static long task2() {
-        int a = 21;
-        long b = 222;
-        System.out.println(a + b);
+    public static int task2() {
+        int a = 212;
+        long b = 22222222;
+        System.out.println( a * b);
         return 0;
     }
 
+    public static void getMaxNumber () {
+        int a = 100;
+        int b = 140;
+        if (a>b){
+            System.out.println("Максимум a = " + a);
+        }
+        else if (b>a){
+            System.out.println("Максимум b = " + b);
+        }
+        else {
+            System.out.println("Цифры равны");
+        }
 
+    }
 
+    public static boolean isCharA() {
+        char a = 'А';
+        switch (a){
+            case 'A': return true;
+            default:
+                return false;
+        }
 
+    }
 
 }
