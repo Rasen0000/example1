d8dc47f484c18f4c2d72d6e612223b36c1451218
Rasen0000 <rasenou@ya.ru>
2020-10-31 12:33:35 +0300

    1

--- src/ru/example/Main.java
+++ src/ru/example/Main.java
@@ -3,7 +3,29 @@ package ru.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.exit(2);
+        //System.exit(2);
+
+        task1();
+        task2();
+
     }
+
+    public static byte task1() {
+        byte a = 2;
+        int b = 1002;
+        System.out.println((byte) (a + b));
+        return 0;
+    }
+
+    public static long task2() {
+        int a = 21;
+        long b = 222;
+        System.out.println(a + b);
+        return 0;
+    }
+
+
+
+
 
 }
